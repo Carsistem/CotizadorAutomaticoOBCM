7f3d61beb5f6b13e6b62ca9ffab2ba6e0159fc17
Carsistem <34581229+Carsistem@users.noreply.github.com>
2017-12-15 14:45:45 -0300

    Add files via upload

---
+++ CotizadorAutomaticoOBCM_Data/Business/Moneda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CotizadorAutomaticoOBCM_Data;
+
+namespace CotizadorAutomaticoOBCM.Business
+{
+    public class Moneda
+    {
+        public static List<MONEDA> getMonedas()
+        {
+            List<MONEDA> lista = new List<MONEDA>();
+            using (Entidades ctx = new Entidades())
+            {
+                foreach (MONEDA mon in ctx.MONEDA.ToList())
+                {
+                    MONEDA item = new MONEDA();
+                    item.IDMONEDA = mon.IDMONEDA;
+                    item.DESCRIPCION = mon.DESCRIPCION;
+                    item.SIGNO = mon.SIGNO;
+                    lista.Add(item);
+                }
+            }
+            return lista;
+        }
+    }
+}
